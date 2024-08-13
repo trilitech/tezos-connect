1fc7166dc93f2bc932303d066f906b6c5f565bb4
Diana Savatina <diana.savvatina@trili.tech>
2024-08-13 15:49:31 +0100

    fix: changed env to yarn 4.4.0

--- src/beacon_types.ts
+++ src/beacon_types.ts
@@ -1,4 +1,39 @@
-export { TezosOperationType } from "@airgap/beacon-sdk";
+export {
+  TezosOperationType,
+  type PartialTezosDalPublishCommitmentOperation,
+  type PartialTezosDelegationOperation,
+  type PartialTezosIncreasePaidStorageOperation,
+  type PartialTezosRegisterGlobalConstantOperation,
+  type PartialTezosRevealOperation,
+  type PartialTezosSetDepositsLimitOperation,
+  type PartialTezosSmartRollupAddMessagesOperation,
+  type PartialTezosSmartRollupCementOperation,
+  type PartialTezosSmartRollupExecuteOutboxMessageOperation,
+  type PartialTezosSmartRollupOriginateOperation,
+  type PartialTezosSmartRollupPublishOperation,
+  type PartialTezosSmartRollupRecoverBondOperation,
+  type PartialTezosSmartRollupRefuteOperation,
+  type PartialTezosSmartRollupTimeoutOperation,
+  type PartialTezosTransactionOperation,
+  type PartialTezosTransferTicketOperation,
+  type PartialTezosUpdateConsensusKeyOperation,
+  type TezosActivateAccountOperation,
+  type TezosAttestationOperation,
+  type TezosAttestationWithSlotOperation,
+  type TezosBallotOperation,
+  type TezosDoubleAttestationEvidenceOperation,
+  type TezosDoubleBakingEvidenceOperation,
+  type TezosDoublePreAttestationEvidenceOperation,
+  type TezosDoublePreEndorsementEvidenceOperation,
+  type TezosDrainDelegateOperation,
+  type TezosEndorsementOperation,
+  type TezosEndorsementWithSlotOperation,
+  type TezosFailingNoopOperation,
+  type TezosPreAttestationOperation,
+  type TezosProposalOperation,
+  type TezosSeedNonceRevelationOperation,
+  type TezosVdfRevelationOperation
+ } from "@airgap/beacon-sdk";
 
 import {
   MichelineMichelsonV1Expression,
@@ -12,8 +47,13 @@ export interface ScriptedContracts {
   storage: MichelineMichelsonV1Expression;
 }
 
-export interface PartialTezosOriginationOperationOverride
+export interface TezosOriginationOperationOverride
   extends Omit<TezosOriginationOperation, "script"> {
+  script: ScriptedContracts;
+}
+
+export interface PartialTezosOriginationOperationOverride
+  extends Omit<PartialTezosOriginationOperation, "script"> {
   script: ScriptedContracts;
 }
 
@@ -22,3 +62,4 @@ type PartialTezosOperationOverride =
   | PartialTezosOriginationOperationOverride;
 
 export { type PartialTezosOperationOverride as PartialTezosOperation };
+export { type PartialTezosOriginationOperationOverride as PartialTezosOriginationOperation };
